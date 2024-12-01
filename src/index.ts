94aea0c2fef023cadaa4485eb4c37630fb36d4fa
Guo <guo.yansong@optimind.tech>
2024-12-01 11:17:45 +0900

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { Loader } from "@/lib/components/EditorLayout/Loader";
 export { EditorControlHeader } from "@/lib/components/EditorLayout/EditorControlHeader";
-export { EditorMainLayput as Main } from "@/lib/components/EditorLayout/EditorMainLayput";
+export { EditorMainLayput } from "@/lib/components/EditorLayout/EditorMainLayput";
